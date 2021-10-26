091d82fdc7f18361f49c8aeb2c4432833b1d6be1
plainsight16 <64688744+plainsight16@users.noreply.github.com>
2021-10-26 22:39:02 +0100

    removed system preference

--- app/js/script.js
+++ app/js/script.js
@@ -2,10 +2,6 @@ const toggle__light = document.getElementById("toggle__light");
 const toggle__dark = document.getElementById("toggle__dark");
 const body = document.querySelector("body");
 
-window.matchMedia("(prefers-color-scheme: dark")
-.addEventListener("change",(event) =>{
-    event.matches ? toggle__dark.click() : toggle__light.click();
-})
 
 const setDarkMode = () => {
     body.classList.add("dark");
